8b0d4031894889ce822f616a1a7e5a12177ae31c
Deckstop <5621kvs@gmail.com>
2023-01-11 11:24:17 +0300

    add model

--- WordApp/MainPage.xaml.cs
+++ WordApp/MainPage.xaml.cs
@@ -11,7 +11,24 @@ public partial class MainPage : ContentPage
 
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
-	
+	List<ModelTask> tasks = new List<ModelTask>();
+        tasks.Add(new ModelTask());
+
 	}
+    public class ModelTask
+    {
+         List<string> data = new List<string>();
+         List<TypeData> typeData = new List<TypeData>();
+        public void AddElemrntTask(string dataText, TypeData typeDataText)
+        {
+            this.data.Add(dataText);
+            this.typeData.Add(typeDataText);
+        }
+    }
+    public enum TypeData
+    {
+        Image,
+        Text
+    }
 }
 
